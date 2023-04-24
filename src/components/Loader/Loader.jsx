a86560ef56af39b91d1bfeccd28ebd5482ab1bc3
n-ruzhik <anastasiyaruzhetskaya@gmail.com>
2023-04-24 15:46:40 +0300

    fixed Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -3,15 +3,17 @@ import css from './Loader.module.css';
 
 export default function Loader() {
   return (
-    <BallTriangle
-      height={100}
-      width={100}
-      radius={5}
-      color="#1DA1F2"
-      ariaLabel="ball-triangle-loading"
-      wrapperClass={css.loader}
-      wrapperStyle=""
-      visible={true}
-    />
+    <div className={css.loader}>
+      <BallTriangle
+        height={100}
+        width={100}
+        radius={5}
+        color="#1DA1F2"
+        ariaLabel="ball-triangle-loading"
+        // wrapperClass={css.loader}
+        wrapperStyle=""
+        visible={true}
+      />
+    </div>
   );
 }
